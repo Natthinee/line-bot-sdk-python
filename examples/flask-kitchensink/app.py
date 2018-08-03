e9075e4a3acbd4af3d0231830e8f9c6b328f88e1
khim ki khim <CHANLONN_N@hotmail.com>
2018-08-04 01:39:43 +0700

    Update app.py

--- examples/flask-kitchensink/app.py
+++ examples/flask-kitchensink/app.py
@@ -12,9 +12,6 @@
 #  License for the specific language governing permissions and limitations
 #  under the License.
 
-from __future__ import unicode_literals
-
-import errno
 import os
 import sys
 import tempfile
